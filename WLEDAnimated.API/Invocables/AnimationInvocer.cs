5021e87a9cf9eab4dd8ffafd76c0cf4ad073a35a
Rob Chartier <rob.chartier@ethersolutions.ca>
2024-01-18 11:29:49 -0800

    basic scheduler to run animations

---
+++ WLEDAnimated.API/Invocables/AnimationInvocer.cs
@@ -0,0 +1,21 @@
+﻿using Coravel.Invocable;
+
+namespace WLEDAnimated.API.Invocables;
+
+public class AnimationInvocer : IInvocable
+{
+    private readonly AnimationManager _animationManager;
+
+    public AnimationInvocer(AnimationManager animationManager)
+    {
+        _animationManager = animationManager;
+    }
+
+    public string Animation { get; set; }
+
+    public async Task Invoke()
+    {
+        Console.WriteLine($"AnimationInvocer '{Animation}' invoked!");
+        await _animationManager.PlayAnimation(Animation);
+    }
+}
--- WLEDAnimated.API/Program.cs
+++ WLEDAnimated.API/Program.cs
@@ -1,61 +1,98 @@
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
+using Coravel;
+using Coravel.Invocable;
 using WLEDAnimated.Animation;
+using WLEDAnimated.API.Invocables;
 using WLEDAnimated.Interfaces;
 
-namespace WLEDAnimated.API
+namespace WLEDAnimated.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<AssetManager>();
+        builder.Services.AddTransient<WLEDApiManager>();
+        builder.Services.AddTransient<ImageUDPSender>();
+        builder.Services.AddTransient<IImageConverter, ImageToDNRGBConverter>();
+        builder.Services.AddTransient<IImageResizer, ImageSharpImageResizer>();
+        builder.Services.AddTransient<IWLEDApiManager, WLEDApiManager>();
+
+        builder.Services.AddTransient<DeviceDiscovery>();
+        builder.Services.AddSingleton<WledDeviceDiscovery>();
+
+        builder.Services.AddTransient<WLEDAnimationLoader>();
+        builder.Services.AddTransient<AnimationManager>();
+        builder.Services.AddTransient<AnimationInvocer>();
+        builder.Services.AddScheduler();
+        builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
+
+        var app = builder.Build();
+
+        //            if (app.Environment.IsDevelopment())
+        //{
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = string.Empty; // To serve the Swagger UI at the app's root
+        });
+        //}
 
-            // Add services to the container.
+        app.UseHttpsRedirection();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddSingleton<AssetManager>();
-            builder.Services.AddTransient<WLEDApiManager>();
-            builder.Services.AddTransient<ImageUDPSender>();
-            builder.Services.AddTransient<IImageConverter, ImageToDNRGBConverter>();
-            builder.Services.AddTransient<IImageResizer, ImageSharpImageResizer>();
-            builder.Services.AddTransient<IWLEDApiManager, WLEDApiManager>();
+        app.UseAuthorization();
 
-            builder.Services.AddTransient<DeviceDiscovery>();
-            builder.Services.AddSingleton<WledDeviceDiscovery>();
+        app.MapControllers();
 
-            builder.Services.AddTransient<WLEDAnimationLoader>();
-            builder.Services.AddTransient<AnimationManager>();
-            
-            builder.Services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
+        app.UseDefaultFiles();
 
-            var app = builder.Build();
-            
-            //            if (app.Environment.IsDevelopment())
-            //{
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
+        var provider = app.Services;
+        provider.UseScheduler(scheduler =>
+        {
+            var config = provider.GetService<IConfiguration>();
+            var schedules = new List<SchedulerConfig>();
+            config.GetSection("Scheduler").Bind(schedules);
+
+            foreach (var schedulerConfig in schedules)
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty; // To serve the Swagger UI at the app's root
-            });
-            //}
+                var type = schedulerConfig.Invocable;
+                var invocer = typeof(Program).Assembly.GetTypes().Where(x => x.FullName.Equals(type, StringComparison.InvariantCultureIgnoreCase))?.FirstOrDefault();
+                if (invocer != null)
+                {
+                    var instance = app.Services.GetService(invocer) as IInvocable;
+                    //var instance = ActivatorUtilities.CreateInstance(provider, invocer) as IInvocable;
+                    var animationInvocer = instance as AnimationInvocer;
 
-            app.UseHttpsRedirection();
+                    if (animationInvocer != null)
+                    {
+                        animationInvocer.Animation = schedulerConfig.Animation;
+                        scheduler.ScheduleAsync(async () => { await instance.Invoke(); }).Cron(schedulerConfig.Cron);
+                    }
+                }
+            }
+        });
 
-            app.UseAuthorization();
+        var dd = app.Services.GetService<WledDeviceDiscovery>();
+        var discover = app.Services.GetService<DeviceDiscovery>();
+        dd.Start(discover);
 
-            app.MapControllers();
-
-            app.UseDefaultFiles();
-
-            var dd = app.Services.GetService<WledDeviceDiscovery>();
-            var discover = app.Services.GetService<DeviceDiscovery>();
-            dd.Start(discover);
-
-            app.Run();
-        }
+        app.Run();
     }
+}
+
+public class SchedulerConfig
+{
+    public string Cron { get; set; } = "*/5 * * * *";
+    public string Invocable { get; set; }
+    public string Animation { get; set; }
 }
