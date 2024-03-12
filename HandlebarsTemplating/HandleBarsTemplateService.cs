b058d5b5aab793814d8338f69a1be43001556d57
Rob Chartier <rob.chartier@ethersolutions.ca>
2024-03-12 16:09:16 -0700

    more logging

--- HandlebarsTemplating/HandleBarsTemplateService.cs
+++ HandlebarsTemplating/HandleBarsTemplateService.cs
@@ -28,9 +28,12 @@ public class HandleBarsTemplateService : ITemplateService
             //we want to use the either a local path, a remote url or the template directly.
             if (text.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase) || text.StartsWith("https://"))
             {
+                _logger.LogInformation("Attemping to downloading template over http.  Source:{Url}");
+
                 Uri uri = null;
                 if (Uri.TryCreate(text, UriKind.RelativeOrAbsolute, out uri))
                 {
+                    _logger.LogInformation("We have a valid URL.  Source:{uri}", uri.ToString());
                     var hash = MD5.Create().ComputeHash(System.Text.Encoding.UTF32.GetBytes(text));
                     var parentFolder = System.IO.Path.Combine(Path.GetTempPath(), "WLEDAnimated.API\\Animations");
                     if (!System.IO.Directory.Exists(parentFolder))
@@ -38,8 +41,12 @@ public class HandleBarsTemplateService : ITemplateService
                         System.IO.Directory.CreateDirectory(parentFolder);
                     }
                     var file = System.IO.Path.Combine(parentFolder, BitConverter.ToString(hash).Replace("-", "") + ".tmpl");
+
+                    _logger.LogInformation("Local file will be:{file}", file);
+
                     if (!System.IO.File.Exists(file))
                     {
+                        _logger.LogInformation("Downloading template over http.  Source:{Url}, Destination:{file}", text, file);
                         var client = new HttpClient();
                         var tmpl = await client.GetStringAsync(text);
                         if (!string.IsNullOrWhiteSpace(tmpl))
@@ -50,6 +57,7 @@ public class HandleBarsTemplateService : ITemplateService
                     }
                     else
                     {
+                        _logger.LogInformation("Downloading template over http.  Destination:{file} already exists. Use it.", file);
                         text = System.IO.File.ReadAllText(file);
                     }
                 };
@@ -58,11 +66,15 @@ public class HandleBarsTemplateService : ITemplateService
             {
                 var animationsFolder = System.IO.Path.Combine(AppContext.BaseDirectory, "Animations");
                 var filePath = System.IO.Path.Combine(animationsFolder, text);
+                _logger.LogInformation("Attempting to load our template from disk:{filePath}", filePath);
+
                 if (System.IO.File.Exists(filePath))
                 {
                     text = System.IO.File.ReadAllText(filePath);
+                    _logger.LogInformation("Template file loaded from disk:{filePath}", filePath);
                 }
             }
+            _logger.LogInformation("Compiling our template:{text}", text);
             var t = Handlebars.Compile(text);
             var result = t(state);
             _logger.LogInformation("Template compiled and executed: '{result}'", result);
--- run-windows.bat
+++ run-windows.bat
@@ -1,1 +1,3 @@
+docker pull robchartier/wledanimateapi
 docker run -d --restart unless-stopped -p 8080:8080/tcp -v %cd%/Schedule.json:/app/Schedule.json -v %cd%/Animations:/app/Animations -v %cd%/appsettings.json:/app/appsettings.json  --name wledanimateapi robchartier/wledanimateapi
+docker ps -a
